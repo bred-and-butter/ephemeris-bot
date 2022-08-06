d7c90d3be075e7a85a580312633d892e8db5f3bf
Frederico <fredericolarangeira@hotmail.com>
2022-08-06 14:55:25 -0300

    bot agora cria e checa por tarefas periodicamente

--- src/bot_instance/__init__.py
+++ src/bot_instance/__init__.py
@@ -4,5 +4,4 @@ from consts import PREFIX
 bot = Bot(command_prefix=PREFIX)
 
 
-from .events import on_ready
 from .commands import *
--- src/jobs.py
+++ src/jobs.py
@@ -5,6 +5,10 @@ import schedule
 
 from date_info.provider import provide_today_info
 
-def create_schedules():
-    schedule.every().day.at("08:00").do(provide_today_info)
 
+def create_jobs():
+    #schedule.every().day.at("08:00").do(provide_today_info)
+    schedule.every(6).seconds.do(print_provider)
+
+def print_provider():
+    print(f"{datetime.now().isoformat()}\n{provide_today_info()}")
